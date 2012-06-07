242acedea7018cc25296f4b62553f12267461684
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-06-08 00:15:08 +0200

    Refine/cleanup declaration vs. definition slides

--- slides/cpp-code/decl-defn.cpp
+++ slides/cpp-code/decl-defn.cpp
@@ -3,9 +3,9 @@ extern const int c = 1;      // defines c
 int f(int x) { return x+a; } // defines f and defines x
 struct S { int a; int b; };  // defines S, S::a, and S::b
 struct X {                   // defines X
-	int x;                   // defines nonstatic data member x
-	static int y;            // declares static data member y
-	X(): x(0) { }            // defines a constructor of X
+    int x;                   // defines nonstatic data member x
+    static int y;            // declares static data member y
+    X(): x(0) { }            // defines a constructor of X
 };
 int X::y = 1;                // defines X::y
 enum { up, down };           // defines up and down
