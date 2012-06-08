6943d0456ef43323e7ffb978ddec21480918fe32
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-06-08 02:22:28 +0200

    Reduce identation by one level to fit better to the slide width

--- slides/cpp-code/overloading-unary-operators.cpp
+++ slides/cpp-code/overloading-unary-operators.cpp
@@ -1,52 +1,52 @@
 //+ unary non-assignment member function
-	class OtherClass { /* ... */ };
-	class MyClass {
-	public:
-		RETURN_TYPE operator@ () { /* ... */ }
-	};
+class OtherClass { /* ... */ };
+class MyClass {
+public:
+	RETURN_TYPE operator@ () { /* ... */ }
+};
 
-	MyClass a;
-	RETURN_TYPE result = @a;
+MyClass a;
+RETURN_TYPE result = @a;
 //-
 
 //+ unary non-assigmnent non-member function
-	class OtherClass { /* ... */ };
-	class MyClass { /* ... */ };
-	
-	RETURN_TYPE operator@ (MyClass p)
-	{ /* ... */ }
+class OtherClass { /* ... */ };
+class MyClass { /* ... */ };
 
-	MyClass a;
-	RETURN_TYPE result = @a;
+RETURN_TYPE operator@ (MyClass p)
+{ /* ... */ }
+
+MyClass a;
+RETURN_TYPE result = @a;
 //-
 
 
 //+ unary suffix member function
-	class OtherClass { /* ... */ };
-	class MyClass {
-	public:
-		MyClass operator@ (int) {
-			MyClass ret = *this;	// copy current state
-			/* modify this instance */
-			return ret;	// return state before modification
-		}
-	};
+class OtherClass { /* ... */ };
+class MyClass {
+public:
+	MyClass operator@ (int) {
+		MyClass ret = *this;	// copy current state
+		/* modify this instance */
+		return ret;	// return state before modification
+	}
+};
 
-	MyClass a;
-	MyClass result = a@;
+MyClass a;
+MyClass result = a@;
 //-
 
 //+ unary suffix non-member function
-	class OtherClass { /* ... */ };
-	class MyClass { /* ... */ };
-	
-	MyClass operator@ (MyClass& p, int)
-	{
-		MyClass ret = p;	// copy current state
-		/* modify p */
-		return ret;	// return state before modification
-	}
+class OtherClass { /* ... */ };
+class MyClass { /* ... */ };
 
-	MyClass a;
-	MyClass result = a@;
+MyClass operator@ (MyClass& p, int)
+{
+	MyClass ret = p;	// copy current state
+	/* modify p */
+	return ret;	// return state before modification
+}
+
+MyClass a;
+MyClass result = a@;
 //-
